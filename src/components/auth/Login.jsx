92f27703b1bf785adb61333e0c13d31709fc8480
abhip9406 <abhip9406@gmail.com>
2026-03-29 10:42:55 +0530

    Update Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
 
         try {
             setLoading(true);
-            const res = await axios.post("http://localhost:3002/login", {
+            const res = await axios.post("http://3.236.164.91:3002/login", {
                 email: email,
                 password: password,
             });
